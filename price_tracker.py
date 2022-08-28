12f1bd951691ccd0f1a75ef42284907731128d42
sneha ramteke <snehavramteke358@gmail.com>
2022-08-28 12:13:56 +0530

    added file to store url

--- price_tracker.py
+++ price_tracker.py
@@ -1,13 +1,29 @@
 import argparse
+from ntpath import join
 arg_parser = argparse.ArgumentParser(description='Price tracker options')
 
-
 arg_parser.add_argument('-a','--add', type=str, help='add a product url')
-
+arg_parser.add_argument('-r','--rmv', type=int, help='remove a product')
+arg_parser.add_argument('-l','--all', action='store_true', help='list all product')
 
 args = arg_parser.parse_args()
 
+DB_PATH = "db.txt"
 
-product_url = args.add;
+def add_to_db(url, db):
+    with open(db, "a") as f:
+        f.writelines(url)
 
+product_url = args.add
+product_id = args.rmv
+product_list = args.all
+
+print(product_list)
 print("found ",product_url)
+print("removed ", product_id )
+
+if(product_url):
+    add_to_db([product_url+"\n"],DB_PATH)
+
+if(product_list):
+    pass
